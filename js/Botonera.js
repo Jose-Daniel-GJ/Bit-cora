bb129a49e06818a831bddbf3da2973d88d3a0034
Jose <josedaniel190604@gmail.com>
2020-09-01 01:04:33 -0500

    responsive

--- js/Botonera.js
+++ js/Botonera.js
@@ -17,7 +17,7 @@ inicio.addEventListener('click', function(){
     cl_1.classList.remove('Active');
 // Parte del header
     titulo.innerHTML = 'Bitácora <br> Aprender a aprender';
-    img.style.backgroundImage ='url(img/graduados.jpg)'
+    img.style.backgroundImage ='url(img/graduados-min.jpg)'
 
 });
 cl_1.addEventListener('click', function(){
@@ -29,5 +29,43 @@ cl_1.addEventListener('click', function(){
     cl_4.classList.remove('Active');
 // Parte del header
     titulo.innerHTML = '¿Qué es el poder?'
-    img.style.backgroundImage ='url(img/libros.jpg)'
+    img.style.backgroundImage ='url(img/libros-min.jpg)'
 });
+
+cl_2.addEventListener('click', function(){
+    // Parte de los botones
+        inicio.classList.remove('Active');
+        cl_1.classList.remove('Active');
+        cl_2.classList.add('Active');
+        cl_3.classList.remove('Active');
+        cl_4.classList.remove('Active');
+    // Parte del header
+        titulo.innerHTML = 'Camino hacia la universidad '
+        img.style.backgroundImage ='url(img/personas_estudiando-min.jpg)'
+    });
+
+cl_3.addEventListener('click', function(){
+        // Parte de los botones
+            inicio.classList.remove('Active');
+            cl_1.classList.remove('Active');
+            cl_2.classList.remove('Active');
+            cl_3.classList.add('Active');
+            cl_4.classList.remove('Active');
+        // Parte del header
+            titulo.innerHTML = 'Mi facultad'
+            img.style.backgroundImage ='url(img/cern-min.jpg)'
+        });
+    
+        // Aqui se modifica la clase 4
+
+cl_4.addEventListener('click', function(){
+   // Parte de los botones
+       inicio.classList.remove('Active');
+       cl_1.classList.remove('Active');
+       cl_2.classList.remove('Active');
+       cl_3.classList.remove('Active');
+       cl_4.classList.add('Active');
+   // Parte del header
+       titulo.innerHTML = 'Titulo'
+   // img.style.backgroundImage ='url(img/cern-min.jpg)'
+ });
